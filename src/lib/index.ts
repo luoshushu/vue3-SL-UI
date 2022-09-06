e6c7f80d4e991f17b77c55841874780ca640fec3
susonglin <susonglin@cmcm.com>
2022-09-06 15:13:01 +0800

    feat: 创建lib/index.ts导出组件

---
+++ src/lib/index.ts
@@ -0,0 +1,14 @@
+/**
+ * export { default as Button } from './Button.vue'
+ * 等价于
+ * 
+ * import Button from './Button.vue'
+ * export { Button }
+ */
+
+export { default as Button } from './Button.vue'
+export { default as Dialog } from './Dialog.vue'
+export { default as Switch } from './Switch.vue'
+export { default as Tabs } from './Tabs.vue'
+export { default as Tab } from './Tab.vue'
+export { openDialog as openDialog } from './openDialog'
